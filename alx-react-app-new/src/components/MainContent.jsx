68617f68f675a58f2c3985e4a151273fa61baf74
oussamaaouam1 <oussama.aouam@gmail.com>
2024-08-15 08:43:05 +0100

    commit the first exer

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{color : 'violet' ,border:'1px dotted' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,10 @@
+const UserProfile = (props) => {
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>{props.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+      <p style={{border:'1px solid red'}}>Bio: {props.bio}</p>
+    </div>
+  );
+};
+export default UserProfile;
