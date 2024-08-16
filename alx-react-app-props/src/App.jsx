a90baf39c43473793c54454b6a3f964f296fe2b8
oussamaaouam1 <oussama.aouam@gmail.com>
2024-08-16 11:32:33 +0100

    commit missing errors capital U in usercentext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
-import userContext from "./components/UserContext";
+import UserContext from "./components/UserContext";
 import ProfilePage from "./components/ProfilePage";
 
 
@@ -45,9 +45,9 @@ function App() {
       
 
 
-    <userContext.Provider value={userData}>
+    <UserContext.Provider value={userData}>
     <ProfilePage />
-    </userContext.Provider>
+    </UserContext.Provider>
 
     </>
   );
