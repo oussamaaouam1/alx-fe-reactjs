452482d63afdac75b6b6850aa9cfbe1985ac480b
oussamaaouam1 <oussama.aouam@gmail.com>
2024-08-18 21:26:15 +0100

    last one

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>
-      <Link to='./'>Home</Link>
+    <div style={{backgroundColor:'white',display:'flex', justifyContent:'space-around' , width:'1000px'} }>
+      <Link to='./' >Home</Link>
       <Link to='./About'>About</Link>
       <Link to='./Contact'>Contact</Link>
       <Link to='./Services'>Services</Link>
