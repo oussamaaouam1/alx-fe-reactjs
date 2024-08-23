a1729a4ffa8e8b07e59e247b8e4d4faa5c76a3d0
oussamaaouam1 <oussama.aouam@gmail.com>
2024-08-23 10:27:51 +0100

    commit test

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,6 +1,6 @@
 // AddRecipeForm component
   import { useState } from 'react';
-  import { useRecipeStore } from '../stores/recipeStore';
+  import { useRecipeStore } from './recipeStore';
 
   const AddRecipeForm = () => {
     const addRecipe = useRecipeStore(state => state.addRecipe);
