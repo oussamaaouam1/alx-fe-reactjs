371d53e46120a3ddd78fdcbe5ebf3dac114b3588
oussamaaouam1 <oussama.aouam@gmail.com>
2024-08-31 14:09:00 +0100

    test changes1

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 //---------------------my code-----------------
 import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './components/PostsComponent'
+import PostsComponent from './components/PostsComponent';
 
 
 
--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -3,24 +3,32 @@ import { useQuery } from 'react-query';
 // Define a fetch function that can be used to fetch data from an API
 const fetchPosts = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
     return res.json();
 };
 
 const PostsComponent = () => {
     // Use the useQuery hook to handle data fetching and caching
-    const { data, isLoading, isError } = useQuery('fetchData', fetchPosts);
+    const { data, isLoading, isError, error } = useQuery('posts', fetchPosts);
+    
 
     // Handle loading state
     if (isLoading) return <div>Loading...</div>;
     // Handle error state
-    if (isError) return <div>Error loading data</div>;
+    if (isError) return 
+    <div>
+      Error loading posts: {error?.message || 'An unknown error occurred.'}
+      </div>;
 
     // Render the fetched data
     return (
         <div>
-            {data.map(item => (
-                <div key={item.id}>{item.name}</div>
+            {data.map((post) => (
+                <div key={post.id}>{post.title}</div>
             ))}
+            
         </div>
     );
 };
