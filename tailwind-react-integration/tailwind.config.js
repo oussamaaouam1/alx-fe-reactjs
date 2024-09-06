5ddf5ced029fa4aff9f55c43fc0be28728f42a31
oussamaaouam1 <oussama.aouam@gmail.com>
2024-09-06 12:49:07 +0100

    test 0

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],  // Replaces 'purge'
+  darkMode: 'class',  // Change this if you use dark mode ('media' or 'class')
+  theme: {
+    extend: {},  // You can extend the theme here
+  },
+  plugins: [],  // Add Tailwind plugins if you have any
+};
