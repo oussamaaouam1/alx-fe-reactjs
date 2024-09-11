7a3c1a086b14a8b187c49ee09546630506134fa4
oussamaaouam1 <oussama.aouam@gmail.com>
2024-09-11 22:21:00 +0100

    fix needs

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,10 +12,10 @@ const HomePage = () => {
   return (
     <div>
       <h1>Recipes</h1>
-      <div className="flex-autol">
+      <div className="sm:grid grid-cols-1 md:grid-cols-2 ">
         {recipes.map((recipe) => (
-          <div key={recipe.id} className="bg-gray-100 p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs hover:shadow-xl cursor-pointer">
-            <img src={recipe.image} className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out bg-blue-300"/>
+          <div key={recipe.id} className="bg-gray-100 p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs hover:shadow-xl cursor-pointer mx-10 ">
+            <img src={recipe.image} alt={recipe.title} className=" bg-blue-400 rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"/>
             <h2 className="text-blue-800">{recipe.title}</h2>
             <p className="text-black">{recipe.summary}</p>
           </div>
