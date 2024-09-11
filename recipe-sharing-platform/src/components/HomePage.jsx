a6b164f98ee7b4eb4314493f432d4fd1537e5b8c
oussamaaouam1 <oussama.aouam@gmail.com>
2024-09-11 22:24:17 +0100

    fix needs

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
   return (
     <div>
       <h1>Recipes</h1>
-      <div className="sm:grid grid-cols-1 md:grid-cols-2 ">
+      <div className="sm:grid grid-cols-1 md:grid-cols-2 lg:grid grid-cols-3">
         {recipes.map((recipe) => (
           <div key={recipe.id} className="bg-gray-100 p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs hover:shadow-xl cursor-pointer mx-10 ">
             <img src={recipe.image} alt={recipe.title} className=" bg-blue-400 rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"/>
