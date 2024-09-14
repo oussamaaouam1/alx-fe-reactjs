312a78a1dc22c739b34f62f00f37dabc7f7e5a3d
oussamaaouam1 <oussama.aouam@gmail.com>
2024-09-14 14:14:00 +0100

    test the last exercice addform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import HomePage from './components/HomePage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeDetail from './components/RecipeDetail'; 
-import AddRecipeForm from './components/AddRecipeFom';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
 
