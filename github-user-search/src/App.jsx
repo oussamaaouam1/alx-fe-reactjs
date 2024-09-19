9fbde04541e6d689af04570b90832c1bc418b93e
oussamaaouam1 <oussama.aouam@gmail.com>
2024-09-19 21:48:17 +0100

    test repo

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" >
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
